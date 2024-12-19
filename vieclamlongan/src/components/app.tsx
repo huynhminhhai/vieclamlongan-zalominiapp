dca76977362ab71f884664376bc83784e6e327ca
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-12-19 16:52:33 +0700

    setup project

---
+++ vieclamlongan/src/components/app.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { App, ZMPRouter, AnimationRoutes, SnackbarProvider } from "zmp-ui";
+import { RecoilRoot } from "recoil";
+import HomePage from "pages/index/index";
+const MyApp = () => {
+  return (
+    <RecoilRoot>
+      <App>
+        <SnackbarProvider>
+          <ZMPRouter>
+            <AnimationRoutes>
+              <Route path="/" element={<HomePage></HomePage>}></Route>
+            </AnimationRoutes>
+          </ZMPRouter>
+        </SnackbarProvider>
+      </App>
+    </RecoilRoot>
+  );
+};
+export default MyApp;
---
+++ vieclamlongan/src/pages/index/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Page } from "zmp-ui";
+import { Welcome } from "./welcome";
+
+const HomePage: React.FunctionComponent = () => {
+    return (
+      <Page className="relative flex-1 flex flex-col bg-white">
+        <Welcome />
+        <Box className="flex-1 overflow-auto">
+
+        </Box>
+      </Page>
+    );
+  };
+  
+  export default HomePage;
